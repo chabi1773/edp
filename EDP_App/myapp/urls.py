420396ce2554df049181e64ca2540bfd69ca2938
chamod <chamod1773@gmail.com>
2025-04-09 18:14:46 +0530

    cahnged for app creation

--- EDP_App/myapp/urls.py
+++ EDP_App/myapp/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import UserProfileViewSet, TemperatureRecordViewSet
-
+#from rest_framework.routers import DefaultRouter
+#from .views import UserProfileViewSet, TemperatureRecordViewSet
+from . import views
+'''
 router = DefaultRouter()
 router.register(r'users', UserProfileViewSet)
 router.register(r'temperatures', TemperatureRecordViewSet)
+'''
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('api/', views.get_users),
+    path('api/user/<int:user_id>', views.get_user_data)
 ]
--- EDP_App/myapp/views.py
+++ EDP_App/myapp/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 from .models import UserProfile, TemperatureRecord
-from rest_framework import viewsets
-from .serializers import UserProfileSerializer, TemperatureRecordSerializer
+#from rest_framework import viewsets
+#from .serializers import UserProfileSerializer, TemperatureRecordSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
+
+ '''
 class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
@@ -10,4 +14,30 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 class TemperatureRecordViewSet(viewsets.ModelViewSet):
     queryset = TemperatureRecord.objects.all()
     serializer_class = TemperatureRecordSerializer
+ '''
+@api_view(['GET'])
+def get_users(request):
+    users = UserProfile.objects.all()
+    data = [{'id':u.user_id, 'name': u.user_name} for u in users]
+    return Response(data)
 
+
+@api_view(['GET'])
+def get_user_data(request, user_id):
+    try:
+        user = UserProfile.objects.get(pk = user_id)
+    except UserProfile.DoesNotExist:
+        return Response({'error': 'User not found'}, status = 404)
+
+    records = TemperatureRecord.objects.filter(user = user).order_by('-record_time')
+    record_list = [
+        {'temperature': r.temperature, 'record_time': r.record_time} for r in records
+    ]
+    current_temp = record_list[0] if record_list else None
+
+    return Response({
+        'user': user.user_name
+        'current_temp': current_temp
+        'records': record_list
+
+    })
